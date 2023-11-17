b8acbaeb866f2c1e8e14fccd41bc290684e66b2d
sak89 <sascha.guida@gmail.com>
2023-11-17 09:16:13 +0100

    fixed style and API custom

--- ReactMidLevel/cinehub/utils/api.js
+++ ReactMidLevel/cinehub/utils/api.js
@@ -1,8 +1,10 @@
-/* export const BASE_URL = 'https://mttlioitimpeuzlwsgql.supabase.co/rest/v1/movies';
+export const BASE_URL = 'https://eyiynmcjpvmgdvsekzby.supabase.co/rest/v1/movie';
+export const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5aXlubWNqcHZtZ2R2c2VremJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMDUyMjcsImV4cCI6MjAxNTc4MTIyN30.83Q_oTuMriKG5kw5VGWuWcnaOsXaekD0_ZbSezwNIiY';
+
 export const HEADERS = {
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer xxx ',
+        'apikey': API_KEY,
     },
 };
 
@@ -18,19 +20,24 @@ export const fetchMovies = async () => {
             throw new Error('Failed to fetch movies');
         }
 
-        const extractedMovies = data.moviesCollection.edges.map((edge) => edge.node);
+        const extractedMovies = data.map((movie) => ({
+            id: movie.id,
+            title: movie.title,
+            description: movie.description,
+            img: movie.img,
+        }));
 
         return extractedMovies;
     } catch (error) {
         console.error('Error fetching movies:', error.message);
         throw error;
     }
-}; */
+};
 
 
-// errore con api fornita , sembra che la KEY non funzioni, test fatto con api pokemon
 
-export const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+// api.js
+/* export const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
 
 export const fetchPokemon = async (pokemonId) => {
     try {
@@ -40,7 +47,6 @@ export const fetchPokemon = async (pokemonId) => {
         }
         const data = await response.json();
 
-        // Log the entire API response
         console.log('API Response:', data);
 
         return data;
@@ -48,4 +54,4 @@ export const fetchPokemon = async (pokemonId) => {
         console.error('Error fetching Pokemon data:', error.message);
         throw error;
     }
-};
+}; */
